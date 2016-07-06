4ecaa1b48e0722dc3ccd71cc7d6f7b1dffeb2eae
Parth Shah <parths.97@gmail.com>
2016-07-05 22:02:20 -0700

    added search integration

--- MoneyPool/InviteFriendsListViewController.swift
+++ MoneyPool/InviteFriendsListViewController.swift
@@ -10,25 +10,23 @@ import UIKit
 
 class InviteFriendsListViewController: UIViewController {
 
+	@IBOutlet weak var searchBar: UISearchBar!
 	@IBOutlet weak var tableView: UITableView!
 	
-	let friends: [String] = ["John", "Tom", "Harry"]
+	let allFriends: [String] = ["John", "Tom", "Harry"]
+	var displayFriends: [String] = []
 	let friendsImages: [UIImage] = []
-	let invitedFriends = [String]()
+	var invitedFriends = [String]()
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		tableView.delegate = self
+		displayFriends = allFriends
+		
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
+	    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -41,17 +39,45 @@ class InviteFriendsListViewController: UIViewController {
 
 }
 
+//MARK: Table View
 extension InviteFriendsListViewController: UITableViewDataSource, UITableViewDelegate{
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return friends.count
+		return displayFriends.count
 	}
 	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! InviteFriendListTableViewCell
-		cell.friendNameLabel.text = friends[indexPath.row]
+		cell.friendNameLabel.text = displayFriends[indexPath.row]
 		//need to set the image of the friend
 		return cell
 	}
 	
 	
 }
+
+//MARK: Search Bar
+extension InviteFriendsListViewController: UISearchBarDelegate{
+	func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+		searchBar.setShowsCancelButton(true, animated: true)
+		//displayFriends = allFriends
+		//tableView.reloadData()
+	}
+	
+	func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+		searchBar.resignFirstResponder()
+		searchBar.text = ""
+		searchBar.setShowsCancelButton(false, animated: true)
+		displayFriends = allFriends
+		tableView.reloadData()
+	}
+	
+	func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+		print(searchText)
+		displayFriends = allFriends.filter(){$0.lowercaseString.containsString(searchText.lowercaseString)}
+		print(displayFriends)
+		tableView.reloadData()
+	}
+	
+}
+
+
