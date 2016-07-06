803dbc90f981c353bb6e2ff7d8d59e6a10a6d686
Jorge Bastos <jbastosneto@me.com>
2016-07-05 18:04:37 -0700

    modifications
    replaced GoogleService-Info.plist with the new server information,
    added file FirebaseHelper.swift to implement all the API’s to use
    firebase in the project
    added file for the model Pool struct
    changed the firebase server

---
+++ MoneyPool/Helpers/FirebaseHelper.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseHelper.swift
+//  MoneyPool
+//
+//  Created by Jorge Bastos on 7/5/16.
+//  Copyright © 2016 Jorge Bastos. All rights reserved.
+//
+
+import Firebase
+
+class FirebaseHelper {
+    // TODO: need implementation
+}
---
+++ MoneyPool/Model/Pool.swift
@@ -0,0 +1,13 @@
+//
+//  Pool.swift
+//  MoneyPool
+//
+//  Created by Jorge Bastos on 7/5/16.
+//  Copyright © 2016 Jorge Bastos. All rights reserved.
+//
+
+import Foundation
+
+struct Pool {
+    
+}
