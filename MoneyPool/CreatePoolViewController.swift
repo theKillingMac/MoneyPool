0a8a3f8e602390c16f355a2cc9c2e7f6f93a36df
edrick <edrickepascual@gmail.com>
2016-07-06 15:27:40 -0700

    MoneyPool Outlets finished
    Outlets Created.

--- MoneyPool/CreatePoolViewController.swift
+++ MoneyPool/CreatePoolViewController.swift
@@ -11,19 +11,54 @@ import UIKit
 class CreatePoolViewController: UIViewController {
     
     
+    @IBOutlet weak var eventName: UITextField!
+    
+    @IBOutlet weak var amountGoal: UITextField!
+    
+    
+    
     @IBOutlet weak var paymentSelection: UIPickerView!
     
+    @IBAction func inviteFriendsTapped(sender: AnyObject) {
+    }
     
     let pickerData = ["daily","weekly","bi-weekly","monthly","bi-monthly", "custom"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
+    }
+    
+    // Segue to invite friends
+    // Prepare for Segue
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            
+            if identifier == "inviteFriends" {
+                print("Invite Cell Tapped")
+                
+                let displayInviteViewController = segue.destinationViewController as! InviteFriendsListViewController
+            
+                
+                
+//                //MARK: Determining the selected note
+//                //1
+//                let indexPath = tableView.indexPathForSelectedRow!
+//                //2 Identifying each cell with the indexpath to retrieve the note from notes array.
+//                let note = notes[indexPath.row]
+//                print(notes)
+          
+            } else {
+                print("error")
+                
+            }
+        }
     }
 
 
+
 }
+
 
 extension CreatePoolViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
--- MoneyPool/Model/Pool.swift
+++ MoneyPool/Model/Pool.swift
@@ -10,6 +10,8 @@ import Foundation
 
 struct Pool: MoneyPoolType, FirebaseConvertible {
     
+    
+    
     init(info: [String : AnyObject]) {
         // TODO: implement
     }
