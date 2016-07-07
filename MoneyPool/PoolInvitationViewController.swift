d67c2ed0478e412a48ca7ce629725413c2722661
Parth Shah <parths.97@gmail.com>
2016-07-06 22:49:44 -0700

    Added outlets and connections to the PoolInvitationViewController

--- MoneyPool/PoolInvitationViewController.swift
+++ MoneyPool/PoolInvitationViewController.swift
@@ -10,17 +10,48 @@ import UIKit
 
 class PoolInvitationViewController: UIViewController {
 
+	@IBOutlet weak var poolInfo: UITextView!
+	@IBOutlet weak var amountToRaise: UILabel!
+	@IBOutlet weak var numberOfPayments: UILabel!
+	@IBOutlet weak var recurringAmount: UILabel!
+	@IBOutlet weak var paymentsMadeEveryTimePeriod: UILabel!
+	
+	var poolTitle: String = "" {
+		didSet{
+			navigationItem.title = poolTitle
+		}
+		
+	}
+	
+	let currentUser:String = "user-123456"
+	
+	//The invitation should be set during the segue to this controller
+	var invitation: Invitation!{
+		didSet{
+			
+		}
+	}
+	
+	@IBOutlet weak var tableView: UITableView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	
+	
+	@IBAction func acceptOrDeclineButtonPushed(sender: UIButton) {
+		if sender.titleLabel?.text == "ACCEPT"{
+			
+			
+			
+		} else if sender.titleLabel?.text == "DECLINE"{
+			
+			
+		}
+		
+	}
 
     /*
     // MARK: - Navigation
